dfa14fe0421bae1edecc1053e0e8b810f8c8f645
slawke5685 <slawekzet@interia.pl>
2018-04-18 21:31:47 +0200

    formularz logowania

--- app/screen/loginForm.js
+++ app/screen/loginForm.js
@@ -4,14 +4,81 @@ import {
     StyleSheet,
     Text,
     View,
+    Button,
 } from 'react-native';
+
+import t from 'tcomb-form-native';
+
+const Form = t.form.Form;
+
+const User = t.struct({
+    email: t.String,
+    password: t.String,
+});
+
+const formStyles = {
+    ...Form.stylesheet,
+    formGroup: {
+        normal: {
+            marginBottom: 10
+        },
+    },
+    controlLabel: {
+        normal: {
+            color: 'blue',
+            fontSize: 18,
+            marginBottom: 7,
+            fontWeight: '600'
+        },
+        // the style applied when a validation error occours
+        error: {
+            color: 'red',
+            fontSize: 18,
+            marginBottom: 7,
+            fontWeight: '600'
+        }
+    }
+};
+
+const options = {
+    fields: {
+        email: {
+            error: 'Email address jest wymagany.'
+        },
+        password: {
+            label: 'Hasło',
+            error: 'Hasło jest wymagane.'
+        },
+    },
+    stylesheet: formStyles,
+};
+
 export default class loginForm extends React.Component{
     static navigationOptions = {
         title: 'Logowanie',
     };
 
+    handleSubmit = () => {
+        const value = this._form.getValue(); // use that ref to get the form value
+        console.log('value: ', value);
+    };
 
     render() {
+        const {navigate} = this.props.navigation;
+
+        return (
+            <View style={styles.container}>
+                <Form ref={c => this._form = c} type={User} options={options}/>
+                <Button
+                    title="Zaloguj!"
+                    onPress={this.handleSubmit}
+                />
+                <Text onPress={() => navigate('profilePage')}>Przejdz dalej do profilu</Text>
+                <Text onPress={() => navigate('mainPage')}>Przejdz dalej do glownego widoku</Text>
+            </View>
+        );
+    }
+    /*render() {
         const {navigate} = this.props.navigation;
         return (
             <View style={styles.container}>
@@ -22,6 +89,14 @@ export default class loginForm extends React.Component{
                 <Text onPress={() => navigate('mainPage')}>Przejdz dalej do glownego widoku</Text>
             </View>
         );
-    }
-
+    }*/
 }
+
+const styles = StyleSheet.create({
+    container: {
+        justifyContent: 'center',
+        marginTop: 50,
+        padding: 20,
+        backgroundColor: '#ffffff',
+    },
+});

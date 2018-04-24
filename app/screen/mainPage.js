5f43c9ecd2be7f7136c5582d44992807272fa0a1
Wojciech Rzemieniuk <wojciechrzemieniuk@gmail.com>
2018-04-24 12:06:35 +0200

    added list to map view

--- app/screen/mainPage.js
+++ app/screen/mainPage.js
@@ -2,8 +2,9 @@ import React from 'react';
 import MapView from 'react-native-maps';
 
 
-import {Dimensions, Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {Button, Dimensions, FlatList, Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 
+const imageURL = require('./../assets/img/icon/50x50/accident.png');
 
 export default class mainPage extends React.Component {
     static navigationOptions = {
@@ -25,7 +26,7 @@ export default class mainPage extends React.Component {
                     },
                     title: 'UEK',
                     description: 'wykolejenie zajęć',
-                    image: require('./../assets/img/icon/accident.png')
+
                 },
                 {
                     latlng: {
@@ -34,7 +35,7 @@ export default class mainPage extends React.Component {
                     },
                     title: "Wypadek",
                     description: "kolizja osobowki i przegubowca z 501. Autobus w połowie blokuje pas w kierunku Armii Krajowej.",
-                    image: require('./../assets/img/icon/accident.png')
+                    image: imageURL
                 }
 
             ],
@@ -66,11 +67,15 @@ export default class mainPage extends React.Component {
                     <Image style={styles.icon} source={require('./../assets/img/menu/hamburger.png')}/>
                 </TouchableOpacity>
 
+                <TouchableOpacity style={styles.eventButtonPosition} onPress={() => navigate('event')}>
+                    <Button title='Dodaj' color="#047024" onPress={() => navigate('event')}/>
+                </TouchableOpacity>
+
                 <MapView style={styles.map}
                          initialRegion={{
                              latitude: 50.0697015,
                              longitude: 19.9521732,
-                             latitudeDelta: 0.0922,
+                             latitudeDelta: 0.03,
                              longitudeDelta: 0.0421,
                          }}
                          showsUserLocation={true}
@@ -78,18 +83,17 @@ export default class mainPage extends React.Component {
                          showsTraffic={true}
                          customMapStyle={require('./../assets/config/style/mapStyle.json')}
                 >
-                    {this.state.markers.map(marker => (
-                        <MapView.Marker
-                            coordinate={marker.latlng}
-                            title={marker.title}
-                            description={marker.description}
-                        >
-                            <Image
-                                source={marker.image}
-                                style={{width: 50, height: 50}}
-                            />
-                        </MapView.Marker>
-                    ))}
+                    <View style={{height: 500, width: 500}}>
+                        {this.state.markers.map(marker => (
+                            <MapView.Marker
+                                coordinate={marker.latlng}
+                                title={marker.title}
+                                description={marker.description}
+                                image={marker.image}
+                            >
+                            </MapView.Marker>))
+                        }
+                    </View>
                     <MapView.Marker
                         coordinate={{
                             latitude: 50.1684783,
@@ -98,16 +102,36 @@ export default class mainPage extends React.Component {
                         title="Wypadek"
                         description="kolizja osobowki i przegubowca z 501. Autobus w połowie blokuje pas w kierunku Armii Krajowej."
                     >
-                        <MapView.Callout>
-                            <TouchableOpacity onPress={() => navigate('DrawerOpen')}>
-                                <Text>Klik</Text>
-                            </TouchableOpacity>
-                        </MapView.Callout>
+
                     </MapView.Marker>
 
-
                 </MapView>
-
+                <FlatList style={styles.list}
+                          data={[
+                              {key: 'Wypadek1'},
+                              {key: 'Wypadek2'},
+                              {key: 'Wypadek3'},
+                              {key: 'Wypadek4'},
+                              {key: 'Wypadek5'},
+                              {key: 'Wypadek6'},
+                              {key: 'Wypadek7'},
+                              {key: 'Wypadek8'},
+                          ]}
+                          renderItem={({item}) => {
+                              return (
+                                  <View style={styles.item}>
+                                      <View>
+                                          <Text style={styles.text}>{item.key}</Text>
+                                          <Text style={styles.description}>Lorem ipsum dolor sit amet, consectetur
+                                              adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+                                              magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                                              laboris nisi ut aliquip ex ea commodo consequat.</Text>
+                                      </View>
+                                  </View>
+                              )
+                          }
+                          }
+                />
 
             </View>
         );
@@ -118,19 +142,17 @@ export default class mainPage extends React.Component {
 
 const styles = StyleSheet.create({
     container: {
-        ...StyleSheet.absoluteFillObject,
-        position: 'absolute',
         top: 0,
         left: 0,
         right: 0,
         bottom: 0,
         justifyContent: 'flex-end',
         alignItems: 'center',
-        backgroundColor: '#68ed89'
+        backgroundColor: '#4f4e4a'
     },
     map: {
-        ...StyleSheet.absoluteFillObject,
-        height: Dimensions.get('window').height / 1.5,
+
+        height: Dimensions.get('window').height / 2,
         width: Dimensions.get('window').width,
     },
     button: {
@@ -140,6 +162,7 @@ const styles = StyleSheet.create({
     icon: {
         width: 30,
         height: 30,
+        paddingTop: 10
     },
 
     iconPosition: {
@@ -147,7 +170,49 @@ const styles = StyleSheet.create({
         top: 30,
         left: 30,
         zIndex: 2
+    },
+    list: {
+        marginTop: -30,
+        shadowOpacity: 0.5,
+        shadowRadius: 15,
+    },
+    item: {
+
+        height: 44,
+        backgroundColor: '#71f793',
+        height: 100,
+        width: Dimensions.get('window').width - 10,
+        marginBottom: 20,
+
+
+        borderWidth: 1,
+        borderRadius: 2,
+        borderColor: '#ddd',
+        borderBottomWidth: 0,
+        shadowColor: '#000',
+        shadowOffset: {width: 0, height: 2},
+        shadowOpacity: 0.8,
+        shadowRadius: 2,
+        elevation: 1,
+        marginLeft: 5,
+        marginRight: 10,
+        marginTop: 10,
+    },
+    text: {
+        fontSize: 18,
+        color: '#235'
+    },
+    description: {
+        fontSize: 12,
+
     }
-});
+    ,
+    eventButtonPosition: {
+        position: 'absolute',
+        top: 10,
+        right: 10,
+        zIndex: 2,
+    },
+})
 
 
